5222a06a7c41daeaf368901ff6beccd5dbf98f14
NooreZannat <160593961+NooreZannat@users.noreply.github.com>
2024-03-06 03:31:08 -0800

    Add files via upload

---
+++ 14-basic class obj.cpp
@@ -0,0 +1,15 @@
+
+// Basic Input-Output operation using class and objects;
+#include<bits/stdc++.h>
+using namespace std;
+class tests{
+    int x = 10;
+    public:
+    void display(){
+        cout<<"The vaule of X is: "<<x<<endl;
+    }
+};
+int main(){
+    tests t;
+    t.display();
+}
